9fab2a4d4dcec8b8d39d8f5a922a4285762e5f8d
SantCorrea802 <santiago.correa7@udea.edu.co>
2026-03-25 12:22:23 -0500

    Actualizar detalles

--- coffee_backend/src/main/java/com/example/coffee/repository/CreditsTransactionRepository.java
+++ coffee_backend/src/main/java/com/example/coffee/repository/CreditsTransactionRepository.java
@@ -4,11 +4,13 @@ import com.example.coffee.entity.CreditsTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+
 
 @Repository
 public interface CreditsTransactionRepository extends JpaRepository<CreditsTransaction, Long> {
     Optional<CreditsTransaction> findById(Long id);
-    Optional<CreditsTransaction> findBySenderAccountNumberOrReceiverAccountNumber(String senderAccountNumber,
-                                                                                  String receiverAccountNumber);
+    List<CreditsTransaction> findBySenderAccountNumberOrReceiverAccountNumber(String senderAccountNumber,
+                                                                              String receiverAccountNumber);
 }
