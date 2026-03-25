ba9cde459eb062a2d2a9961a0b54f8ed181b3934
SantCorrea802 <santiago.correa7@udea.edu.co>
2026-03-25 12:23:08 -0500

    Actualizar los services de las transferencias de creditos y dinero

--- coffee_backend/src/main/java/com/example/coffee/service/CreditsTransactionService.java
+++ coffee_backend/src/main/java/com/example/coffee/service/CreditsTransactionService.java
@@ -2,50 +2,115 @@ package com.example.coffee.service;
 
 
 import com.example.coffee.DTO.CreditsTransactionDTO;
-import com.example.coffee.DTO.MoneyTransactionDTO;
 import com.example.coffee.entity.CreditsTransaction;
+import com.example.coffee.entity.Customer;
 import com.example.coffee.mapper.CreditsTransactionMapper;
 import com.example.coffee.repository.CreditsTransactionRepository;
+import com.example.coffee.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 public class CreditsTransactionService {
 
+    private final CustomerRepository customerRepository;
     private final CreditsTransactionRepository creditsTransactionRepository;
     private final CreditsTransactionMapper creditsTransactionMapper;
 
     @Autowired
-    public CreditsTransactionService(CreditsTransactionRepository creditsTransactionRepository, CreditsTransactionMapper creditsTransactionMapper) {
+    public CreditsTransactionService(CustomerRepository customerRepository, CreditsTransactionRepository creditsTransactionRepository, CreditsTransactionMapper creditsTransactionMapper) {
+        this.customerRepository = customerRepository;
         this.creditsTransactionRepository = creditsTransactionRepository;
         this.creditsTransactionMapper = creditsTransactionMapper;
     }
 
+    public CreditsTransactionDTO transferCredits(CreditsTransactionDTO creditsTransactionDTO){
+        if(creditsTransactionDTO.getSenderAccountNumber() == null || creditsTransactionDTO.getReceiverAccountNumber() == null){
+            throw new IllegalArgumentException("El número de cuenta del remitente y del destinatario no pueden ser nulos.");
+        }
 
-    public CreditsTransactionDTO getCreditsTransactionById(Long id){
-        return creditsTransactionRepository.findById(id).map(creditsTransactionMapper::toDTO).orElseThrow(()->new RuntimeException("Transacción de créditos no encontrada."));
+        Customer sender = customerRepository.findByAccountNumber(creditsTransactionDTO.getSenderAccountNumber()).orElseThrow(()-> new IllegalArgumentException("El remitente no existe."));
+        Customer receiver = customerRepository.findByAccountNumber(creditsTransactionDTO.getReceiverAccountNumber()).orElseThrow(()-> new IllegalArgumentException("El destinatario no existe."));
 
-    }
+        // Comprobamos que el sender tenga suficiente saldo de creditos para realizar la transferencia
+        // se hace uso de compareTo al ser BigDecimal
+        if (sender.getBalance().compareTo(BigDecimal.valueOf(creditsTransactionDTO.getCreditsAmount())) < 0) {
+            throw new IllegalArgumentException("El remitente no tiene suficiente saldo para realizar la transferencia.");
+        }
+        if (creditsTransactionDTO.getCreditsAmount() <= 0) {
+            throw new IllegalArgumentException("El monto de créditos a transferir debe ser mayor a cero.");
+        }
 
-    public CreditsTransactionDTO getCreditsTransactionBySenderOrReceiverAccountNumber(String senderAccountNumber, String receiverAccountNumber){
-        return creditsTransactionRepository.findBySenderAccountNumberOrReceiverAccountNumber(senderAccountNumber, receiverAccountNumber).map(creditsTransactionMapper::toDTO).orElseThrow(()->new RuntimeException("Transacción de créditos no encontrada."));
-    }
+        if (creditsTransactionDTO.getSenderAccountNumber().equals(creditsTransactionDTO.getReceiverAccountNumber())) {
+            throw new IllegalArgumentException("El remitente y el destinatario no pueden ser la misma cuenta.");
+        }
+        // realizar la transferencia de CREDITOS a las cuentas sender y receiver
+        sender.setBalance(sender.getBalance().subtract(BigDecimal.valueOf(creditsTransactionDTO.getCreditsAmount())));
+        receiver.setBalance(receiver.getBalance().add(BigDecimal.valueOf(creditsTransactionDTO.getCreditsAmount())));
 
-    public List<CreditsTransactionDTO> getAllCreditsTransactions(){
-        return creditsTransactionRepository.findAll().stream().map(creditsTransactionMapper::toDTO).toList();
-    }
+        // guardamos los cambios en la base de datos
+        customerRepository.save(sender);
+        customerRepository.save(receiver);
 
+        // Crear la transaccion de dinero y guardarla en la base de datos
+        CreditsTransaction creditsTransaction = new CreditsTransaction();
+        creditsTransaction.setSenderAccountNumber(creditsTransactionDTO.getSenderAccountNumber());
+        creditsTransaction.setReceiverAccountNumber(creditsTransactionDTO.getReceiverAccountNumber());
+        creditsTransaction.setCreditsAmount(creditsTransactionDTO.getCreditsAmount());
+        creditsTransaction.setTimeTransaction(creditsTransactionDTO.getTimeTransaction());
 
-    public CreditsTransactionDTO createCreditsTransaction(CreditsTransactionDTO creditsTransactionDTO){
-        CreditsTransaction creditsTransaction = creditsTransactionMapper.toEntity(creditsTransactionDTO);
+        // devolver la transaccion como dto y guardarla en la base de datos
         return creditsTransactionMapper.toDTO(creditsTransactionRepository.save(creditsTransaction));
     }
 
-    public void deleteCreditsTransaction(Long id){
-        CreditsTransaction creditsTransaction = creditsTransactionRepository.findById(id).orElseThrow(()->new RuntimeException("Transacción de créditos no encontrada."));
-        creditsTransactionRepository.delete(creditsTransaction);
+    // obtener todas las transacciones de dinero realizadas por un cliente a través de su número de cuenta, ya sea como remitente o destinatario
+    public List<CreditsTransactionDTO> getTransactionsByAccountNumber(String accountNumber){
+        List<CreditsTransaction> transactions = creditsTransactionRepository.findBySenderAccountNumberOrReceiverAccountNumber(accountNumber, accountNumber);
+        return transactions.stream().map(creditsTransactionMapper::toDTO).toList();
     }
+
+
+    // obtener una transacción de dinero por su id
+    public CreditsTransactionDTO getTransactionById(Long id){
+        CreditsTransaction creditsTransaction = creditsTransactionRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("La transacción no existe."));
+        return creditsTransactionMapper.toDTO(creditsTransaction);
+    }
+
+
+    //Obtener todas las transacciones de dinero realizadas en el sistema
+    public List<CreditsTransactionDTO> getAllTransactions(){
+        List<CreditsTransaction> transactions = creditsTransactionRepository.findAll();
+        return transactions.stream().map(creditsTransactionMapper::toDTO).toList();
+    }
+
+    // actualizar una transacción de dinero por su id, solo se puede actualizar el monto de la transacción, no se pueden actualizar el remitente ni el destinatario
+    public CreditsTransactionDTO updateTransactionAmount(Long id, CreditsTransactionDTO creditsTransactionDTO){
+        if (creditsTransactionDTO.getCreditsAmount() <= 0) {
+            throw new IllegalArgumentException("El monto de créditos a transferir debe ser mayor a cero.");
+        }
+
+        CreditsTransaction creditsTransaction = creditsTransactionRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("La transacción no existe."));
+        if (creditsTransaction.getSenderAccountNumber().equals(creditsTransaction.getReceiverAccountNumber())) {
+            throw new IllegalArgumentException("El remitente y el destinatario no pueden ser la misma cuenta.");
+        }
+        if (creditsTransaction.getCreditsAmount() < 0){
+            throw new IllegalArgumentException("No se puede actualizar el monto de una transacción eliminada.");
+        }
+        creditsTransaction.setCreditsAmount(creditsTransactionDTO.getCreditsAmount());
+        return creditsTransactionMapper.toDTO(creditsTransactionRepository.save(creditsTransaction));
+    }
+
+    // eliminar transaccion de dinero por su id, esto no elimina la transacción de la base de datos, solo la marca como eliminada, para mantener un registro de todas las transacciones realizadas en el sistema
+    public void deleteTransaction(Long id){
+        CreditsTransaction creditsTransaction = creditsTransactionRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("La transacción no existe."));
+        creditsTransaction.setCreditsAmount(-1); // se marca la transacción como eliminada poniendo el monto de créditos en -1, esto es solo una convención
+        creditsTransaction.setSenderAccountNumber(null);
+        creditsTransaction.setReceiverAccountNumber(null);
+        creditsTransactionRepository.save(creditsTransaction);
+    }
+
 
 }
--- coffee_backend/src/main/java/com/example/coffee/service/MoneyTransactionService.java
+++ coffee_backend/src/main/java/com/example/coffee/service/MoneyTransactionService.java
@@ -1,46 +1,118 @@
 package com.example.coffee.service;
 
 import com.example.coffee.DTO.MoneyTransactionDTO;
+import com.example.coffee.entity.Customer;
 import com.example.coffee.entity.MoneyTransaction;
 import com.example.coffee.mapper.MoneyTransactionMapper;
+import com.example.coffee.repository.CustomerRepository;
 import com.example.coffee.repository.MoneyTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 
 @Service
 public class MoneyTransactionService {
 
+    private final CustomerRepository customerRepository;
     private final MoneyTransactionRepository moneyTransactionRepository;
     private final MoneyTransactionMapper moneyTransactionMapper;
 
     @Autowired
-    public MoneyTransactionService(MoneyTransactionRepository moneyTransactionRepository, MoneyTransactionMapper moneyTransactionMapper){
+    public MoneyTransactionService(CustomerRepository customerRepository, MoneyTransactionRepository moneyTransactionRepository, MoneyTransactionMapper moneyTransactionMapper) {
+        this.customerRepository = customerRepository;
         this.moneyTransactionRepository = moneyTransactionRepository;
         this.moneyTransactionMapper = moneyTransactionMapper;
     }
 
-    public MoneyTransactionDTO getMoneyTransactionById(Long id){
-        return moneyTransactionRepository.findById(id).map(moneyTransactionMapper::toDTO).orElseThrow(()->new RuntimeException("Transacción no encontrada."));
-    }
+    public MoneyTransactionDTO transferMoney(MoneyTransactionDTO moneyTransactionDTO){
+        if(moneyTransactionDTO.getSenderAccountNumber() == null || moneyTransactionDTO.getReceiverAccountNumber() == null){
+            throw new IllegalArgumentException("El número de cuenta del remitente y del destinatario no pueden ser nulos.");
+        }
 
-    public MoneyTransactionDTO getMoneyTransactionBySenderOrReceiverAccountNumber(String senderAccountNumber, String receiverAccountNumber){
-        return moneyTransactionRepository.findBySenderAccountNumberOrReceiverAccountNumber(senderAccountNumber, receiverAccountNumber).map(moneyTransactionMapper::toDTO).orElseThrow(()->new RuntimeException("Transacción no encontrada."));
-    }
+        Customer sender = customerRepository.findByAccountNumber(moneyTransactionDTO.getSenderAccountNumber()).orElseThrow(()-> new IllegalArgumentException("El remitente no existe."));
+        Customer receiver = customerRepository.findByAccountNumber(moneyTransactionDTO.getReceiverAccountNumber()).orElseThrow(()-> new IllegalArgumentException("El destinatario no existe."));
 
-    public List<MoneyTransactionDTO> getAllMoneyTransactions(){
-        return moneyTransactionRepository.findAll().stream().map(moneyTransactionMapper::toDTO).toList();
-    }
+        // Comprobamos que el sender tenga suficiente saldo para realizar la transferencia
+        // se hace uso de compareTo al ser BigDecimal
+        if (sender.getBalance().compareTo(moneyTransactionDTO.getAmount()) < 0) {
+            throw new IllegalArgumentException("El remitente no tiene suficiente saldo para realizar la transferencia.");
+        }
+        if (moneyTransactionDTO.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("El monto a transferir debe ser mayor a cero.");
+        }
+        if (moneyTransactionDTO.getSenderAccountNumber().equals(moneyTransactionDTO.getReceiverAccountNumber())) {
+            throw new IllegalArgumentException("El remitente y el destinatario no pueden ser la misma cuenta.");
+        }
 
-     public MoneyTransactionDTO createMoneyTransaction(MoneyTransactionDTO moneyTransactionDTO){
-        MoneyTransaction moneyTransaction = moneyTransactionMapper.toEntity(moneyTransactionDTO);
+
+        // realizar la transferencia del dinero a las cuentas sender y receiver
+        sender.setBalance(sender.getBalance().subtract(moneyTransactionDTO.getAmount()));
+        receiver.setBalance(receiver.getBalance().add(moneyTransactionDTO.getAmount()));
+
+        // guardamos los cambios en la base de datos
+        customerRepository.save(sender);
+        customerRepository.save(receiver);
+
+        // Crear la transaccion de dinero y guardarla en la base de datos
+        MoneyTransaction moneyTransaction = new MoneyTransaction();
+        moneyTransaction.setSenderAccountNumber(moneyTransactionDTO.getSenderAccountNumber());
+        moneyTransaction.setReceiverAccountNumber(moneyTransactionDTO.getReceiverAccountNumber());
+        moneyTransaction.setAmount(moneyTransactionDTO.getAmount());
+        moneyTransaction.setTimeTransaction(moneyTransactionDTO.getTimeTransaction());
+
+        // devolver la transaccion como dto y guardarla en la base de datos
         return moneyTransactionMapper.toDTO(moneyTransactionRepository.save(moneyTransaction));
     }
 
-    public void deleteMoneyTransaction(Long id){
-        MoneyTransaction moneyTransaction = moneyTransactionRepository.findById(id).orElseThrow(()->new RuntimeException("Transacción no encontrada."));
-        moneyTransactionRepository.delete(moneyTransaction);
+    // obtener todas las transacciones de dinero realizadas por un cliente a través de su número de cuenta, ya sea como remitente o destinatario
+    public List<MoneyTransactionDTO> getTransactionsByAccountNumber(String accountNumber){
+        List<MoneyTransaction> transactions = moneyTransactionRepository.findBySenderAccountNumberOrReceiverAccountNumber(accountNumber, accountNumber);
+        return transactions.stream().map(moneyTransactionMapper::toDTO).toList();
     }
+
+
+    // obtener una transacción de dinero por su id
+    public MoneyTransactionDTO getTransactionById(Long id){
+        MoneyTransaction moneyTransaction = moneyTransactionRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("La transacción no existe."));
+        return moneyTransactionMapper.toDTO(moneyTransaction);
+    }
+
+
+    //Obtener todas las transacciones de dinero realizadas en el sistema
+    public List<MoneyTransactionDTO> getAllTransactions(){
+        List<MoneyTransaction> transactions = moneyTransactionRepository.findAll();
+        return transactions.stream().map(moneyTransactionMapper::toDTO).toList();
+    }
+
+    // actualizar una transacción de dinero por su id, solo se puede actualizar el monto de la transacción, no se pueden actualizar el remitente ni el destinatario
+    public MoneyTransactionDTO updateTransactionAmount(Long id, MoneyTransactionDTO moneyTransactionDTO){
+        if(moneyTransactionDTO.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("El monto a transferir debe ser mayor a cero.");
+        }
+        MoneyTransaction moneyTransaction = moneyTransactionRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("La transacción no existe."));
+        if(moneyTransaction.getSenderAccountNumber().equals(moneyTransaction.getReceiverAccountNumber())) {
+            throw new IllegalArgumentException("El remitente y el destinatario no pueden ser la misma cuenta.");
+        }
+        // verificar si no es una transaccion eliminada (amount == null) para evitar actualizar una transaccion eliminada
+        if(moneyTransaction.getAmount() == null) {
+            throw new IllegalArgumentException("No se puede actualizar el monto de una transacción eliminada.");
+        }
+        moneyTransaction.setAmount(moneyTransactionDTO.getAmount());
+        return moneyTransactionMapper.toDTO(moneyTransactionRepository.save(moneyTransaction));
+    }
+
+    // eliminar transaccion de dinero por su id, esto no elimina la transacción de la base de datos, solo la marca como eliminada, para mantener un registro de todas las transacciones realizadas en el sistema
+    public void deleteTransaction(Long id){
+        MoneyTransaction moneyTransaction = moneyTransactionRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("La transacción no existe."));
+        moneyTransaction.setAmount(null);
+        moneyTransaction.setSenderAccountNumber(null);
+        moneyTransaction.setReceiverAccountNumber(null);
+        moneyTransactionRepository.save(moneyTransaction);
+    }
+
+
+
 }
